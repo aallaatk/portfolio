c3014912e5579ab8eddbcfd71fade55b3260d428
unknown <sleimiala@gmail.com*@example.com>
2024-11-24 14:46:03 +0100

    customer feedbacks fixes

--- port/src/Sections/Testimonials.tsx
+++ port/src/Sections/Testimonials.tsx
@@ -1,22 +1,24 @@
 import Testimonial from "../Components/Testimonial";
 import Title from "../Components/Title";
-
+import img1 from "../assets/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L2pvYjEwMzQtZWxlbWVudC0wNy00MDMucG5n.webp";
+import img2 from "../assets/p2.webp";
+import img3 from "../assets/p3.jfif";
 
 const testimonialsData = [
   {
-    img: 'https://via.placeholder.com/100',
+    img: img1,
     name: 'Richard Miles',
     title: 'Chairman',
     quote: 'Outstanding Work!',
   },
   {
-    img: 'https://via.placeholder.com/100',
+    img: img3,
     name: 'Vesta Shufelt',
     title: 'Executive Director',
     quote: 'Highly Skilled and Reliable!',
   },
   {
-    img: 'https://via.placeholder.com/100',
+    img: img2,
     name: 'Joseph Alves',
     title: 'Managing Director',
     quote: 'Exceptional Developer!',
